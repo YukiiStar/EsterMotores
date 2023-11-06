45681b60ac3e5b7ff67511a7b60b0239fcdeda77
ESTER LOYSSE MEDEIROS PEREIRA <20221170150009@ifrn.local>
2023-11-06 14:30:42 -0300

    Jogo das Moedas

--- Novo jogo/Assets/Scripts/Moeda.cs
+++ Novo jogo/Assets/Scripts/Moeda.cs
@@ -12,8 +12,9 @@ public class Moeda : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
+            FindObjectOfType<GameManager>().SubtrairMoedas(1);
             Destroy(gameObject);
         }
     }
--- Novo jogo/Assets/Scripts/player.cs
+++ Novo jogo/Assets/Scripts/player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
     public int velocidade = 10;
